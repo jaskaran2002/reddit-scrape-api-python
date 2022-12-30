a96ee50dcb2eab1d98a63a3f6723945a900a2b36
simardeepsm <smudhar_be21@thapar.edu>
2022-12-30 21:31:50 +0530

    commit-2

--- main.py
+++ main.py
@@ -51,6 +51,58 @@ with open('./test.json', 'w') as f:
     json.dump(data,f,indent=4)
 
 
+@app.route('/runDuckPython/<string:query>')
+def searchNews(query):
+    PATH = "./geckodriver"
+    data = []
+    options = Options()
+    PATH_TO_DEV_NULL = '/dev/null'
+    options.headless = True
+    driver = webdriver.Firefox(executable_path=PATH, options=options, service_log_path=PATH_TO_DEV_NULL)
+    # driver = webdriver.Firefox(executable_path=PATH)
+    url = f"https://duckduckgo.com/{query}"
+    driver.get(url)
+    # search = driver.find_element(By.ID, 'searchbox_input')
+    # search.send_keys(query)
+    # search.send_keys(Keys.RETURN)
+    try:
+        news = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.LINK_TEXT, 'News')))
+    except:
+        print("Not Found")
+        driver.quit()
+        exit()
+    news.click()
+    try:
+        cards = WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'result__body')))
+    except:
+        print("Cards not found")
+        driver.quit()
+        exit()
+    for card in cards:
+        info = {}
+        info['title'] = card.find_element(By.CLASS_NAME, 'result__a').text
+        info['text'] = card.find_element(By.CLASS_NAME, 'result__snippet').text
+        website = card.find_element(By.CLASS_NAME, 'result__url')
+        info['website'] = website.text
+        info['news-link'] = website.get_attribute('href')
+        try:
+            img = card.find_element(By.CLASS_NAME, 'result__image__img')
+            info['image'] = img.get_attribute('src')
+        except:
+            print("not Found")
+        data.append(info)
+    driver.quit()
+    return data
+
+
+# query = ' '.join(sys.argv[1:])
+# query="duckirony"
+output = searchNews(query)
+with open('output.json', 'w') as f:
+    json.dump(output,f,indent=4)
+
+
+    
 
 
 if __name__ == '__main__':
